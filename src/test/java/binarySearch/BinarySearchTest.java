8cf469302e126e9bb1162afdd0d652d1fa1945a9
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-05-29 11:52:10 -0700

    test complete

---
+++ src/test/java/binarySearch/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package binarySearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BinarySearchTest {
+
+    @Test
+    public void testSomeLibraryMethod() {
+        BinarySearch classUnderTest = new BinarySearch();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
