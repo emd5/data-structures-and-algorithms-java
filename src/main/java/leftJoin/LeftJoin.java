08f008d9aa7fdad9aecfede7ecd20977cab4ab84
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-07-11 17:44:57 -0700

    test complete, readme updated;

--- src/main/java/leftJoin/LeftJoin.java
+++ src/main/java/leftJoin/LeftJoin.java
@@ -1,7 +1,5 @@
 package leftJoin;
 
-import hashTable.HashTable;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,22 +19,4 @@ public class LeftJoin {
 
         return list;
     }
-
-    public static void main(String[] args) {
-        HashMap h1 = new HashMap();
-        h1.put("fond", "enamored");
-        h1.put("wrath", "anger");
-        h1.put("diligent", "employed");
-        h1.put("outfit", "garb");
-        h1.put("guide", "usher");
-
-        HashMap h2 = new HashMap();
-        h2.put("fond", "averse");
-        h2.put("wrath", "delight");
-        h2.put("flow", "jam");
-
-        System.out.println(LeftJoin.leftJoin(h1, h2));
-    }
-
-
 }
